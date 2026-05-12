6a7404474f2e45b65130801ee6a4725ee4a8b9c2
ismolinska <ismolinska@edu.cdv.pl>
2026-05-12 19:43:14 +0200

    AHDSR implementation

--- Assets/Scripts/Outside_foot_switch.cs
+++ Assets/Scripts/Outside_foot_switch.cs
@@ -44,7 +44,7 @@ public class Outside_foot_switch : MonoBehaviour
                 ToggleSnapshot(true);
             }
             // Wyłącza snapshot, jeśli gracz jest wewnątrz, a snapshot jest aktywny.
-            else if ((tag == "Inside_stone" || tag == "Inside_wood") && snapshotActivated)
+            else if ((tag == "Outside" || tag == "Stone") && snapshotActivated)
             {
                 ToggleSnapshot(false);
             }
